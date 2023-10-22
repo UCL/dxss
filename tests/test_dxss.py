389a08902d73ac21953dc93da947f0010dafaa33
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-10-22 18:32:43 +0100

    Explicitly make PyPardiso an optional dependency.
    And handle ImportError as recommended on stackoverflow
    (missing_library = None). Also add some warnings for missing pypardiso
    if users instantiate a PySolver. Still very WET for the moment.
    
    Probably makes #18 moot.

--- tests/test_dxss.py
+++ tests/test_dxss.py
@@ -1,6 +1,21 @@
 """Tests for dxss package."""
 
+import pytest
 
 def test_dummy():
     """Dummy test - just try importing dxss."""
     import dxss  # noqa: F401
+
+
+def test_pardiso():
+    """Test for PyPardiso. Print info for `pytest -s` logging."""
+    from dxss.solve_1d import pypardiso, PySolver
+
+    if pypardiso is not None:
+        import pypardiso as _  # noqa: F401
+
+        print("PyPardiso is available.")
+    else:
+        print("PyPardiso is not available.")
+        with pytest.warns(UserWarning):
+            pypardiso_solver = PySolver(Asp=None, psolver=None)
