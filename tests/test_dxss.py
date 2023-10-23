5ca4306bdafeb7bd715ae8d3bdffc94ba298f534
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-10-23 10:35:02 +0100

    Test pypardiso import checking mechanics.

--- tests/test_dxss.py
+++ tests/test_dxss.py
@@ -1,5 +1,7 @@
 """Tests for dxss package."""
 
+import sys
+
 import pytest
 
 
@@ -20,3 +22,41 @@ def test_pardiso():
         print("PyPardiso is not available.")
         with pytest.warns(UserWarning):
             PySolver(Asp=None, psolver=None)
+
+
+def test_mock_no_pypardiso_for_solve_1d(mocker):
+    # mock no pypardiso installed (even on systems where it's installed)
+    mocker.patch.dict(sys.modules, {"pypardiso": None})
+
+    # check that trying to import raises an ImportError
+    with pytest.raises(ImportError):
+        import pypardiso
+
+    # now check that solve_1d gracefully handles missing pypardiso and warns
+    from dxss.solve_1d import PySolver, pypardiso  # noqa: F811
+
+    assert pypardiso is None
+    with pytest.warns(UserWarning):
+        PySolver(Asp=None, psolver=None)
+
+
+def test_mock_no_pypardiso_for_solve_2d(mocker):
+    mocker.patch.dict(sys.modules, {"pypardiso": None})
+    with pytest.raises(ImportError):
+        import pypardiso
+    from dxss.solve_2d import PySolver, pypardiso  # noqa: F811
+
+    assert pypardiso is None
+    with pytest.warns(UserWarning):
+        PySolver(Asp=None, psolver=None)
+
+
+def test_mock_no_pypardiso_for_solve_3d(mocker):
+    mocker.patch.dict(sys.modules, {"pypardiso": None})
+    with pytest.raises(ImportError):
+        import pypardiso
+    from dxss.solve_3d import PySolver, pypardiso  # noqa: F811
+
+    assert pypardiso is None
+    with pytest.warns(UserWarning):
+        PySolver(Asp=None, psolver=None)
