117f2507832ae9447e6c6aa6edb47fe1abcda86a
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2023-11-07 11:39:35 +0000

    Fix unused pre and alignment bug in docstrings.

--- src/dxss/gmres.py
+++ src/dxss/gmres.py
@@ -5,19 +5,20 @@ from dxss.space_time import SpaceTime
 
 class PreTimeMarchingImproved:
     """Class that wraps our research-grade improved time marching precondition.
-    
+
     The class wrapper is needed to get it into the format needed by PETSc's KSP
     interface.
-    
+
     Attributes:
         context: (dxss.SpaceTime) the sparse matrix context containing n, T, ...
-        
-    TODO:
+
+    Todo:
         Ultimately we could move the code really into this class, and
         potentially make some computational savings by splitting the function
         SpaceTime.pre_time_marching_improved into this class's setup and apply.
         Also TODO is to review this docstring.
     """
+
     def setUp(self, pc):
         A_shell, _ = pc.getOperators()  # noqa: N806 | convention for matrix
         self.context = A_shell.getPythonContext()  # retrieve & set context
@@ -27,35 +28,41 @@ class PreTimeMarchingImproved:
 
 
 def convergence_monitor(ksp: PETSc.KSP, its: int, rnorm: float) -> None:  # noqa: ARG001
-     """A simple rate-printing convergence monitor.
+    """A simple rate-printing convergence monitor.
 
-    The function trace is dictated by PETSc. 
-    https://petsc.org/release/petsc4py/reference/petsc4py.typing.KSPMonitorFunction.html  
-      
+    The function trace is dictated by PETSc.
+    https://petsc.org/release/petsc4py/reference/petsc4py.typing.KSPMonitorFunction.html
+
     Args:
         ksp: The PETSc.KSP instance. Currently unused (ARG001). Needed to comply
              with PETSc's monitor function format.
         its: The iteration count passed in by PETSc.
         rnorm: The (estimated) 2-norm of (preconditioned) residual.
-    
-    Note: 
-        The `rnorm` is _not_ the relative normalisation despite its name.
+
+    Note: The `rnorm` is _not_ the relative normalisation despite its name.
     """
     PETSc.Sys.Print(f"GMRes iteration {its:>3}, residual = {rnorm:4.2e}")
 
 
-def shellmult(self, A: SpaceTime.FMatrix, vec_in: PETSc.Vec, vec_out: PETSc.Vec) -> None:  # noqa: N803, ARG001
+def shellmult(
+    self,
+    A: SpaceTime.FMatrix,  # noqa: ARG001,N803
+    vec_in: PETSc.Vec,
+    vec_out: PETSc.Vec,
+) -> None:
     """Shell matrix multiplication wrapper function.
-    
-    Needed to monkey-patch an instance of dxss.SpaceTime to recover the desired behavior.
-    
+
+    Needed to monkey-patch an instance of dxss.SpaceTime to recover the desired
+    behavior.
+
     Args:
         A: The shell representation of the LHS in our 'matrix-free' scheme.
         vec_in: The vector to multiply with A.
         vec_out: The output vector to contain the multiplication result.
-        
-    TODO:
-        Ultimately this should be integrated into the dxss.SpaceTime class during a refactoring pass.
+
+    Todo:
+        Ultimately this should be integrated into the dxss.SpaceTime class
+        during a refactoring pass.
     """
     self.st.apply_spacetime_matrix(vec_in, vec_out)
 
@@ -63,7 +70,7 @@ def shellmult(self, A: SpaceTime.FMatrix, vec_in: PETSc.Vec, vec_out: PETSc.Vec)
 def get_gmres_solution(
     A,  # noqa: N803 | convention: Ax = b
     b,
-    pre=None,  # noqa: ARG001 | we use a custom preconditioner using PETSc's shell matrix functionality
+    pre=None,
     x=None,
     maxsteps=100,
     tol=None,
@@ -101,9 +108,11 @@ def get_gmres_solution(
     ksp.setTolerances(rtol=reltol, atol=tol, max_it=maxsteps)
 
     # Prepare shell preconditioner
+    if pre is None:
+        pre = PreTimeMarchingImproved()
     pc = ksp.pc
     pc.setType(pc.Type.PYTHON)
-    pc.setPythonContext(PreTimeMarchingImproved())
+    pc.setPythonContext(pre)
     pc.setUp()
 
     petsc_opts = PETSc.Options()
